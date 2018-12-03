2699493eb144a335d5d79d349b89ce8b275dfd61
wllw <liu1351853931@139.com>
2018-12-03 10:47:28 +0800

    gen mips modify test.txt

--- C0Complier/out.asm
+++ C0Complier/out.asm
@@ -2,29 +2,37 @@
     global_var: .space 4808
     str_0: .asciiz "Test start:"
     str_1: .asciiz "Test printf:"
-    str_2: .asciiz "This is a String."
-    str_3: .asciiz "_\\n\\r\\t_"
+    str_2: .asciiz "This is a String:"
+    str_3: .asciiz " !\\n_\\r_\\t }~"
     str_4: .asciiz "X*2+x/3 = "
     str_5: .asciiz "Test scanf:"
-    str_6: .asciiz "Please input int a:"
-    str_7: .asciiz "int a is "
-    str_8: .asciiz "Please in\\tput char x:"
-    str_9: .asciiz "char x is "
-    str_10: .asciiz "Test charcomp:"
-    str_11: .asciiz "Test calculate:"
-    str_12: .asciiz "a: "
-    str_13: .asciiz "b: "
-    str_14: .asciiz "a b add: "
-    str_15: .asciiz "a b sub: "
-    str_16: .asciiz "a b mul: "
-    str_17: .asciiz "a b div: "
-    str_18: .asciiz "Test strrev:"
-    str_19: .asciiz "Please input N, n:"
-    str_20: .asciiz "Test factorial N: "
-    str_21: .asciiz "Test fibonacci n: "
-    str_22: .asciiz "Please input 10 numbers:"
-    str_23: .asciiz "Test sort:"
-    str_24: .asciiz "Test finished."
+    str_6: .asciiz "Please input an integer nnn:"
+    str_7: .asciiz "nnn is "
+    str_8: .asciiz "Please input a character x:"
+    str_9: .asciiz "x is "
+    str_10: .asciiz "Please input 2 integers a, b and a character x:"
+    str_11: .asciiz "a is "
+    str_12: .asciiz "b is "
+    str_13: .asciiz "x is "
+    str_14: .asciiz "Test charcomp:"
+    str_15: .asciiz "Test calculate:"
+    str_16: .asciiz "a: "
+    str_17: .asciiz "b: "
+    str_18: .asciiz "a b add: "
+    str_19: .asciiz "a b sub: "
+    str_20: .asciiz "a b mul: "
+    str_21: .asciiz "a b div: "
+    str_22: .asciiz "Test strrev:"
+    str_23: .asciiz "str before reverse:"
+    str_24: .asciiz "str after reverse:"
+    str_25: .asciiz "Test factorial and fibonacci:"
+    str_26: .asciiz "Please input 2 integers N, n:"
+    str_27: .asciiz "factorial N is "
+    str_28: .asciiz "fibonacci n is "
+    str_29: .asciiz "Test sort:"
+    str_30: .asciiz "Please input 10 numbers:"
+    str_31: .asciiz "sort result:"
+    str_32: .asciiz "Test finished."
 
 
 .text
@@ -548,8 +556,6 @@ func_7:
     label_28:
     j label_16
     label_27:
-    li $t0, 0
-    sw $t0, -12($fp)
     label_16:
     lw $t0, -12($fp)
     move $v0, $t0
@@ -642,7 +648,7 @@ func_9:
     syscall
     li $t0, 120
     move $a0, $t0
-    li $v0, 11
+    li $v0, 1
     syscall
     li $a0, 10
     li $v0, 11
@@ -656,7 +662,7 @@ func_9:
     syscall
     li $t0, 88
     move $a0, $t0
-    li $v0, 11
+    li $v0, 1
     syscall
     li $a0, 10
     li $v0, 11
@@ -702,12 +708,12 @@ func_9:
     syscall
     li $v0, 5
     syscall
-    sw $v0, -4($fp)
+    sw $v0, -12($fp)
     la $t0, str_7
     move $a0, $t0
     li $v0, 4
     syscall
-    lw $t0, -4($fp)
+    lw $t0, -12($fp)
     move $a0, $t0
     li $v0, 1
     syscall
@@ -736,6 +742,55 @@ func_9:
     li $v0, 11
     syscall
     la $t0, str_10
+    move $a0, $t0
+    li $v0, 4
+    syscall
+    li $a0, 10
+    li $v0, 11
+    syscall
+    li $v0, 5
+    syscall
+    sw $v0, -4($fp)
+    li $v0, 5
+    syscall
+    sw $v0, -8($fp)
+    li $v0, 12
+    syscall
+    sb $v0, -24($fp)
+    la $t0, str_11
+    move $a0, $t0
+    li $v0, 4
+    syscall
+    lw $t0, -4($fp)
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    li $a0, 10
+    li $v0, 11
+    syscall
+    la $t0, str_12
+    move $a0, $t0
+    li $v0, 4
+    syscall
+    lw $t0, -8($fp)
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    li $a0, 10
+    li $v0, 11
+    syscall
+    la $t0, str_13
+    move $a0, $t0
+    li $v0, 4
+    syscall
+    lb $t0, -24($fp)
+    move $a0, $t0
+    li $v0, 11
+    syscall
+    li $a0, 10
+    li $v0, 11
+    syscall
+    la $t0, str_14
     move $a0, $t0
     li $v0, 4
     syscall
@@ -887,7 +942,7 @@ func_9:
     sw $t0, -16($fp)
     j label_31
     label_32:
-    la $t0, str_11
+    la $t0, str_15
     move $a0, $t0
     li $v0, 4
     syscall
@@ -920,12 +975,12 @@ func_9:
     sw $t0, -8($fp)
     li $t0, 1
     li $t1, 2
-    li $t2, 3
+    lw $t2, -4($fp)
     li $t3, 4
     li $t4, 5
     li $t5, 6
     li $t6, 7
-    li $t7, 8
+    lw $t7, -8($fp)
     addu $t6, $t6, $t7
     addu $t5, $t5, $t6
     addu $t4, $t4, $t5
@@ -934,7 +989,7 @@ func_9:
     addu $t1, $t1, $t2
     addu $t0, $t0, $t1
     sw $t0, -12($fp)
-    la $t0, str_12
+    la $t0, str_16
     move $a0, $t0
     li $v0, 4
     syscall
@@ -945,7 +1000,7 @@ func_9:
     li $a0, 10
     li $v0, 11
     syscall
-    la $t0, str_13
+    la $t0, str_17
     move $a0, $t0
     li $v0, 4
     syscall
@@ -995,7 +1050,7 @@ func_9:
     addiu $sp, $sp, 60
     move $t0, $v0
     sw $t0, -0($fp)
-    la $t0, str_14
+    la $t0, str_18
     move $a0, $t0
     li $v0, 4
     syscall
@@ -1045,7 +1100,7 @@ func_9:
     addiu $sp, $sp, 60
     move $t0, $v0
     sw $t0, -0($fp)
-    la $t0, str_15
+    la $t0, str_19
     move $a0, $t0
     li $v0, 4
     syscall
@@ -1095,7 +1150,7 @@ func_9:
     addiu $sp, $sp, 60
     move $t0, $v0
     sw $t0, -0($fp)
-    la $t0, str_16
+    la $t0, str_20
     move $a0, $t0
     li $v0, 4
     syscall
@@ -1106,7 +1161,7 @@ func_9:
     li $a0, 10
     li $v0, 11
     syscall
-    la $t0, str_17
+    la $t0, str_21
     move $a0, $t0
     li $v0, 4
     syscall
@@ -1154,7 +1209,7 @@ func_9:
     li $a0, 10
     li $v0, 11
     syscall
-    la $t0, str_18
+    la $t0, str_22
     move $a0, $t0
     li $v0, 4
     syscall
@@ -1197,6 +1252,35 @@ func_9:
     li $t1, 51
     sll $t0, $t0, 2
     sb $t1, global_var+808($t0)
+    la $t0, str_23
+    move $a0, $t0
+    li $v0, 4
+    syscall
+    li $a0, 10
+    li $v0, 11
+    syscall
+    li $t0, 0
+    sw $t0, -16($fp)
+    label_33:
+    lw $t0, -16($fp)
+    li $t1, 9
+    slt $t0, $t0, $t1
+    beq $t0, $0, label_34
+    lw $t0, -16($fp)
+    sll $t0, $t0, 2
+    lb $t0, global_var+808($t0)
+    move $a0, $t0
+    li $v0, 11
+    syscall
+    li $a0, 10
+    li $v0, 11
+    syscall
+    lw $t0, -16($fp)
+    li $t1, 1
+    addu $t0, $t0, $t1
+    sw $t0, -16($fp)
+    j label_33
+    label_34:
     li $t0, 0
     addiu $sp, $sp, -4
     sw $t0, 0($sp)
@@ -1231,13 +1315,20 @@ func_9:
     lw $t8, 40($sp)
     lw $t9, 44($sp)
     addiu $sp, $sp, 60
+    la $t0, str_24
+    move $a0, $t0
+    li $v0, 4
+    syscall
+    li $a0, 10
+    li $v0, 11
+    syscall
     li $t0, 0
     sw $t0, -16($fp)
-    label_33:
+    label_35:
     lw $t0, -16($fp)
     li $t1, 9
     slt $t0, $t0, $t1
-    beq $t0, $0, label_34
+    beq $t0, $0, label_36
     lw $t0, -16($fp)
     sll $t0, $t0, 2
     lb $t0, global_var+808($t0)
@@ -1251,9 +1342,16 @@ func_9:
     li $t1, 1
     addu $t0, $t0, $t1
     sw $t0, -16($fp)
-    j label_33
-    label_34:
-    la $t0, str_19
+    j label_35
+    label_36:
+    la $t0, str_25
+    move $a0, $t0
+    li $v0, 4
+    syscall
+    li $a0, 10
+    li $v0, 11
+    syscall
+    la $t0, str_26
     move $a0, $t0
     li $v0, 4
     syscall
@@ -1266,7 +1364,7 @@ func_9:
     li $v0, 5
     syscall
     sw $v0, global_var+4
-    la $t0, str_20
+    la $t0, str_27
     move $a0, $t0
     li $v0, 4
     syscall
@@ -1336,7 +1434,7 @@ func_9:
     lw $t8, 40($sp)
     lw $t9, 44($sp)
     addiu $sp, $sp, 52
-    la $t0, str_21
+    la $t0, str_28
     move $a0, $t0
     li $v0, 4
     syscall
@@ -1378,7 +1476,14 @@ func_9:
     li $a0, 10
     li $v0, 11
     syscall
-    la $t0, str_22
+    la $t0, str_29
+    move $a0, $t0
+    li $v0, 4
+    syscall
+    li $a0, 10
+    li $v0, 11
+    syscall
+    la $t0, str_30
     move $a0, $t0
     li $v0, 4
     syscall
@@ -1387,11 +1492,11 @@ func_9:
     syscall
     li $t0, 0
     sw $t0, -16($fp)
-    label_35:
+    label_37:
     lw $t0, -16($fp)
     li $t1, 10
     slt $t0, $t0, $t1
-    beq $t0, $0, label_36
+    beq $t0, $0, label_38
     li $v0, 5
     syscall
     sw $v0, -20($fp)
@@ -1403,8 +1508,8 @@ func_9:
     li $t1, 1
     addu $t0, $t0, $t1
     sw $t0, -16($fp)
-    j label_35
-    label_36:
+    j label_37
+    label_38:
     li $t0, 0
     addiu $sp, $sp, -4
     sw $t0, 0($sp)
@@ -1439,7 +1544,7 @@ func_9:
     lw $t8, 40($sp)
     lw $t9, 44($sp)
     addiu $sp, $sp, 64
-    la $t0, str_23
+    la $t0, str_31
     move $a0, $t0
     li $v0, 4
     syscall
@@ -1448,11 +1553,11 @@ func_9:
     syscall
     li $t0, 0
     sw $t0, -16($fp)
-    label_37:
+    label_39:
     lw $t0, -16($fp)
     li $t1, 10
     slt $t0, $t0, $t1
-    beq $t0, $0, label_38
+    beq $t0, $0, label_40
     lw $t0, -16($fp)
     sll $t0, $t0, 2
     lw $t0, global_var+408($t0)
@@ -1466,9 +1571,9 @@ func_9:
     li $t1, 1
     addu $t0, $t0, $t1
     sw $t0, -16($fp)
-    j label_37
-    label_38:
-    la $t0, str_24
+    j label_39
+    label_40:
+    la $t0, str_32
     move $a0, $t0
     li $v0, 4
     syscall
